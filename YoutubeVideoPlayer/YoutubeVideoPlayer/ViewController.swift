315507ae7be49fe14618d56b0ac8cd8a434e6a54
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-07-27 12:14:59 -0500

    Programmatic UI Setup
    - Deleted main.storyboard
    - Setup ViewController as root view controller in SceneDelegate

---
+++ YoutubeVideoPlayer/YoutubeVideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeVideoPlayer
+//
+//  Created by Osaretin Uyigue on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
